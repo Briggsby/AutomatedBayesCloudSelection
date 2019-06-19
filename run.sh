cb447a026b0004fbfa36b64dab7a8cc7ecfe4d04
briggsby <jackbriggsby@gmail.com>
2019-06-19 17:29:00 +0100

    Adding .gitignore

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ cd docker_deploy
 rm terraform.tfstate
 rm terraform.tfstate.backup
 cd ../instance_deploy/aws
+terraform init
 terraform apply -auto-approve
 cd ../../docker_deploy
+terraform init
 terraform apply -auto-approve
